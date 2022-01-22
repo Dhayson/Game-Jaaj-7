c232b4823858699a66afa9b53219d9b06b762790
Dhay <Diego50094@gmail.com>
2022-01-22 17:40:45 -0300

    Add CameraScript.cs

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] private Transform follow;
+    private Transform trans;
+    private Vector3 relativePosition;
+
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+        relativePosition = new Vector3(trans.position.x - follow.position.x, trans.position.y, trans.position.z);
+    }
+
+    void FixedUpdate()
+    {
+        //make the camera follow the transform x axis.
+        trans.position = new Vector3(follow.position.x, 0, 0) + relativePosition;
+    }
+}
