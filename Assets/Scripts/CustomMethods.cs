2c053ad5ecfb1dbe53df68cf1760f37a8bd6674e
Dhay <Diego50094@gmail.com>
2022-01-23 15:44:43 -0300

    Add Global.sizeMultiplier (missing from last commit).

--- Assets/Scripts/CustomMethods.cs
+++ Assets/Scripts/CustomMethods.cs
@@ -18,4 +18,6 @@ public static class Global
 {
     //global field to store the chosen image
     public static string nemesisImagePath;
+    //global size scale
+    public static float sizeMultiplier = 1f;
 }
