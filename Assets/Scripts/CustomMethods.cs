d2e16d87425bf9b1997cf4bb07ed5a6cac9ab5d8
Dhay <Diego50094@gmail.com>
2022-01-23 19:32:54 -0300

    Add sprite editor on SelectVillain scene (Selector prefab). -Add Bound (square to help the editor). -Move Nemesis' SpriteRenderer to a child gameObject. -NemesisSprite.cs: Add methods to edit the sprite. -OpenImage.cs: Set to activate Bound while activate Nemesis.

--- Assets/Scripts/CustomMethods.cs
+++ Assets/Scripts/CustomMethods.cs
@@ -19,5 +19,14 @@ public static class Global
     //global field to store the chosen image
     public static string nemesisImagePath;
     //global size scale
-    public static float sizeMultiplier = 1f;
+    public static Vector2 sizeMultiplier = new Vector2(1, 1);
+
+    /// <summary>
+    /// Return the Raycast of the current mouse position
+    /// </summary>
+    static public RaycastHit2D MouseClick(Camera camera)
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        return Physics2D.Raycast(ray.origin, ray.direction);
+    }
 }
