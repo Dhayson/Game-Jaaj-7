0554c95ef5293b426432f2d1d61ba055040c4833
Dhay <Diego50094@gmail.com>
2022-01-23 21:48:25 -0300

    Add introduction

--- Assets/Scripts/OpenImage.cs
+++ Assets/Scripts/OpenImage.cs
@@ -6,13 +6,11 @@ using UnityEngine.UI;
 
 public class OpenImage : MonoBehaviour
 {
-    private Text[] display;
     [SerializeField] private GameObject Nemesis;
     [SerializeField] private GameObject Bound;
     // Start is called before the first frame update
     void Start()
     {
-        display = GetComponentsInChildren<Text>();
         //game introduction before this
         var extensions = new[]
         {
