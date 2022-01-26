526d6dadbd384367bd173da780b030015c29bcd7
Dhay <Diego50094@gmail.com>
2022-01-26 16:11:06 -0300

    Update how the skills' cooldown is stored in Villain.

--- Assets/Scripts/Villain.cs
+++ Assets/Scripts/Villain.cs
@@ -9,7 +9,11 @@ public class Villain : MonoBehaviour
     [SerializeField] private Vector2 mousePos;
     private VillainControl control;
     [SerializeField] private GameObject ice;
+    [SerializeField] private float geloCD;
     [SerializeField] private GameObject wave;
+    [SerializeField] private float ondaCD;
+    [SerializeField] private GameObject spike;
+    [SerializeField] private float espinhoCD;
 
     void Awake()
     {
@@ -21,44 +25,60 @@ public class Villain : MonoBehaviour
 
         var method2 = thisType.GetMethod(HabilitySet.W.ToString());
         control.Skills.W.started += ctx => method2.Invoke(this, null);
+
+        var method3 = thisType.GetMethod(HabilitySet.R.ToString());
+        control.Skills.R.started += ctx => method3.Invoke(this, null);
         //syntax: control.Skills.Q.started += ctx => Method();
     }
     void Start()
     {
         control.Skills.Enable();
-        geloCDOG = geloCD;
-        geloCD = 0;
-        ondaCDOG = ondaCD;
-        ondaCD = 0;
     }
 
     void FixedUpdate()
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (geloCD > 0) geloCD -= Time.fixedDeltaTime;
-        if (ondaCD > 0) ondaCD -= Time.fixedDeltaTime;
+        if (E_CD > 0) E_CD -= Time.fixedDeltaTime;
+        if (W_CD > 0) W_CD -= Time.fixedDeltaTime;
+
+        if (R_CD > 0) R_CD -= Time.fixedDeltaTime;
     }
 
     public void vazio() { }
 
-    public float geloCD;
-    private float geloCDOG;
+    public float E_CD;
+    private float E_CDog;
     public void gelo()
     {
-        if (geloCD <= 0)
+        E_CDog = geloCD;
+        if (E_CD <= 0)
         {
             Instantiate(ice, mousePos, ice.transform.rotation);
-            geloCD = geloCDOG;
+            E_CD = E_CDog;
         }
     }
-    public float ondaCD;
-    private float ondaCDOG;
+
+    public float W_CD;
+    private float W_CDog;
     public void onda()
     {
-        if (ondaCD <= 0)
+        W_CDog = ondaCD;
+        if (W_CD <= 0)
         {
             Instantiate(wave, mousePos, wave.transform.rotation);
-            ondaCD = ondaCDOG;
+            W_CD = W_CDog;
+        }
+    }
+
+    public float R_CD;
+    private float R_CDog;
+    public void espinho()
+    {
+        R_CDog = espinhoCD;
+        if (R_CD <= 0)
+        {
+            Instantiate(spike, mousePos, spike.transform.rotation);
+            R_CD = R_CDog;
         }
     }
 }
